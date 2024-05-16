3cb24495dd1b5676c382b2f1199448fb37f6276a
yuvraj-jr87 <166809081+yuvraj-jr87@users.noreply.github.com>
2024-05-16 15:49:59 +0530

    Update AppRoutes.js

--- src/components/common/AppRoutes.js
+++ src/components/common/AppRoutes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../user/Login";
 import Parent from "../other/Parent";
 import Employee from '../employee/Employee';
